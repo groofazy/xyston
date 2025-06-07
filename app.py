711d6c9e55df8f6994fe647149f6ba30860095cb
gabe-pc <gabe.gamboa05@gmail.com>
2025-06-07 01:11:13 -0700

    created routes for user auth

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+from flask import Flask, session, redirect, request
+from auth import get_auth_url, generate_code_verifier, SECRET_KEY, exchange_code_for_token
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
+
+@app.route("/")
+def index():
+    return redirect("/login")
+
+# redirect to the auth URL
+@app.route("/login")
+def login():
+    session['verifier'] = generate_code_verifier() # store in Flask session
+    url = get_auth_url(session['verifier'])
+    return redirect(url)
+
+# catch the redirect
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    verifier = session.get("verifier")
+
+    if not code or not verifier:
+        return "Missing code or verifier", 400
+    
+    token_response = exchange_code_for_token(code, verifier)
+
+    if "access_token" in token_response:
+        session['access_token'] = token_response['access_token']
+        return "login successful!"
+    else:
+        return f"Error: {token_response}", 400
